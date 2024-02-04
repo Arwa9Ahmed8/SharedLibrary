49545fa538d0faeeb8ef4ad973ef34ada9357b1d
Arwa9Ahmed8 <93740256+Arwa9Ahmed8@users.noreply.github.com>
2024-02-05 01:22:13 +0200

    Update ComputeStorage.groovy

--- vars/ComputeStorage.groovy
+++ vars/ComputeStorage.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 def call() {
-sh """
+sh '''
 DISK_SIZE_TOTAL=$(df -kh . | tail -n1 | awk '{print $2}')
 echo "DISK_SIZE_TOTAL is $DISK_SIZE_TOTAL"
 DISK_SIZE_FREE=$(df -kh . | tail -n1 | awk '{print $4}')
 echo "DISK_SIZE_FREE is $DISK_SIZE_FREE"
 DISK_PERCENT_USED=$(df -kh . | tail -n1 | awk '{print $5}')
 echo "DISK_PERCENT_USED is $DISK_PERCENT_USED"
-"""
+'''
 }
